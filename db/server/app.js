61332f4641a229a422ea5d441cd3fe106dbc52d4
muthuramanct <ctmuthu93@gmail.com>
2019-10-28 19:50:16 +0100

    Issue #1, #2 , #3 of gitlab

--- db/server/app.js
+++ db/server/app.js
@@ -23,7 +23,7 @@ app.get('/api', (req, res) => {
 	baseUrl: '',
 	endpoints: [
 	  {method: 'GET', path: '/api', description: 'Describes all available endpoints'},
-	  {method: 'GET', path: '/api/tds/', description: 'Get All TD information'},
+	  {method: 'GET', path: '/api/tds/', description: 'Get All latest version TD information'},
 	  {method: 'GET', path: '/api/td/', description: 'Get a TD information'},
 	  {method: 'POST', path: '/api/td/', description: 'Insert a new TD information'},
 	  {method: 'PUT', path: '/api/td/', description: 'Update a TD information, based on id'},
@@ -42,7 +42,7 @@ app.get('/', function(req, res){
  */
 app.get('/api/tds/', async (req, res) => {
   try {
-	var result = await db.find().exec();
+	var result = await db.find({version: {"$eq": 1}}).exec();
 	res.send(result);
   } catch (error) {
 	res.status(500).send(error);
@@ -65,12 +65,20 @@ app.get('/api/td/:id', async (req, res) => {
  */
 app.post('/api/td/', async (req, res) => {
   try {
-	if (req.body.description.id != req.body.id)
-	  res.end("id should be same as description.id");
+	var latestTD;
+	var proceed = 1;
+	req.body.id = req.body.description.id;
+	var existingCount = await db.find({id: req.body.id}).count().exec();
+	if (existingCount === 0)
+	  req.body.version = 1;
 	else {
-	  var existingCount = await db.find({id: req.body.id}).count().exec();
-	  if (existingCount === 0)
-		req.body.version = 1;
+	  latestTD = await db.find({id: req.body.id, version: 1}, {description: 1, _id: 0}).exec();
+	  if(isEquivalent(latestTD[0].description,req.body.description)) {
+		proceed = 0;
+		res.status(500).send("Same as latest TD");
+	  }
+	}
+	if (proceed) {
 	  if (existingCount === 5) {
 		await db.remove({ id: req.body.id, version: 5}).exec();
 		existingCount = 4;
@@ -113,3 +121,29 @@ app.delete('/api/td/:id', async (req, res) => {
 app.listen(process.env.PORT || 3005, () => {
   console.log('Express server is up and running on http://localhost:3000/');
 })
+
+function isEquivalent(a, b) {
+    // Create arrays of property names
+    var aProps = Object.getOwnPropertyNames(a);
+    var bProps = Object.getOwnPropertyNames(b);
+
+    // If number of properties is different,
+    // objects are not equivalent
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+
+    for (var i = 0; i < aProps.length; i++) {
+        var propName = aProps[i];
+
+        // If values of same property are not equal,
+        // objects are not equivalent
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+
+    // If we made it this far, objects
+    // are considered equivalent
+    return true;
+}
--- db/server/model/td.js
+++ db/server/model/td.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose'),
 Schema = mongoose.Schema;
 const TDSchema = new Schema({
-	id: {type:String, required:true}, // ID of the TD
+	id: String, // ID of the TD
 	version: Number,
 	description: {type:Object, required:true}, // Thing Description
 	created: {type: Date, default: Date.now}
-	//id: String,
-	//description: String
  });
 
 const TDModel = mongoose.model('td', TDSchema);
