3621285927675f3eea12a5c37eb56f4321041396
muthuramanct <ctmuthu93@gmail.com>
2019-11-11 14:28:59 +0100

    Added Result table

--- tester.js
+++ tester.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const sleep = require('sleep');
+var results = [];
 async function main() {
   request('http://localhost:3005/api/tds', async (err, res, body) => {
 	if (err) { return console.log(err); }
@@ -8,13 +9,19 @@ async function main() {
 	for (var i = 0; i < tds.length; i++) {
 	  td = tds[i];
 	  console.log(td.id);
-	  td = JSON.stringify(td.td);
+	  tdStr = JSON.stringify(td.td);
 	  url = 'http://localhost:8980/wot-test-bench/actions/fastTest';
-	  body = td;
+	  body = tdStr;
 	  sleep.sleep(5);
-	  let res = await doPost(url, body);
+	  let [res, resBody] = await doPost(url, body);
 	  console.log(res);
+	  results.push({
+		id: td.id,
+		consoleLog: res,
+		errors: "Yet to code",
+		testbenchReport: resBody});
 	}
+	console.log(results);
   });
 }
 function doPost(url, body) {
@@ -22,7 +29,7 @@ function doPost(url, body) {
   return new Promise(function (resolve, reject) {
 	request.post({url,body}, function (error, res, body) {
 	  if (!error && res.statusCode == 200) {
-		resolve(body);
+		resolve([res,body]);
 	  } else {
 		reject(error);
 	  }
